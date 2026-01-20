8dda93d2b59931e68117d984f254df1641f2a455
leharteveld <l.e.harteveld@gmail.com>
2026-01-20 15:54:03 +0100

    bibliotheek opdracht afgemaakt

--- Les 5/Bibliotheek.py
+++ Les 5/Bibliotheek.py
@@ -50,5 +50,59 @@ bibliotheek = {
 # Als je een keuze menu maakt, kun je een while loop gebruiken die blijft loopen totdat de gebruiker kiest om te stoppen.
 # Je keuze menu is eigenlijk een if-elif-else statement. Je kunt de keuze van de gebruiker opslaan in een variabele en dan met if-elif-else bepalen wat er moet gebeuren.
 
+keuze = " "
 
-print("Welkom bij de bibliotheek!")
+while keuze != 4 :
+    print("Welkom bij de bibliotheek!\n",
+      ("*" * 50),"\n"
+      "U kunt een keuze maken uit het volgende menu\n"
+      "1 - Boek toevoegen\n"
+      "2 - Boekenlijst opvragen \n"
+      "3 - Boekenlijst per genre opvragen\n"
+      "4 - Stoppen\n",
+        ("*" * 50),"\n")
+
+
+    keuze = int(input("Maak uw keuze: "))
+
+    if keuze == 4:
+        print("Tot ziens!")
+        break
+    elif keuze == 1:
+        boek = input("Voer de titel van het boek in: ")
+        auteur = input("Voer de naam van de auteur in: ")
+        genre = input("Voer het genre in: ")
+        publicatiejaar = input("Voer het jaar van publicatie in: ")
+
+        bibliotheek[boek] = {
+            "auteur": auteur,
+            "genre": genre,
+            "publicatiejaar": publicatiejaar
+        }
+
+        print(f'Het boek {boek} van {auteur} is toegevoegd aan de bibliotheek')
+        print("*" * 50)
+
+    elif keuze == 2:
+        for boek in bibliotheek:
+            print("*" * 50)
+            print(f"Titel: {boek}")
+            print(f"Auteur: {bibliotheek[boek]["auteur"]}")
+            print(f"Genre: {bibliotheek[boek]["genre"]}")
+            print(f"Publicatiejaar: {bibliotheek[boek]["publicatiejaar"]}")
+    elif keuze == 3:
+        genre = input("Voer het genre in: ")
+        for boek in bibliotheek:
+            if bibliotheek[boek]["genre"]== genre:
+                print("*" * 50)
+                print(f"Titel: {boek}")
+                print(f"Auteur: {bibliotheek[boek]["auteur"]}")
+                print(f"Genre: {bibliotheek[boek]["genre"]}")
+                print(f"Publicatiejaar: {bibliotheek[boek]["publicatiejaar"]}")
+                print("*" * 50)
+
+    else:
+        print("Verkeerde invoer, probeer het nog eens\n")
+
+
+
