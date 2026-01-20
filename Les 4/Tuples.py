ab393f98597b92aff8baec01fa1ae4f750306ebc
leharteveld <l.e.harteveld@gmail.com>
2026-01-20 14:39:21 +0100

    Tuples afgemaakt

--- Les 4/Tuples.py
+++ Les 4/Tuples.py
@@ -7,23 +7,36 @@ tuple1 = (1, 2, 3)
 tuple2 = (4, 5, 6)
 
 # Combineer de twee tuples tot één nieuwe tuple
+nieuwe_tuple = tuple1 + tuple2
 
-
+print(nieuwe_tuple)
 # --------------------------------------------------------------------------------------------
 
 
 # Definieer een tuple met verschillende soorten gegevens (bijvoorbeeld getallen, strings, booleans, etc.)
 
+verschillende_gegevens = ("Hallo", "True", "Wereld", "1")
 
 # Print enkele elementen van de tuple namelijk het eerste, een subset (vanaf index 2 tot index 5) en het laatste element
 
+print(verschillende_gegevens[0])
+print(verschillende_gegevens[2:5])
+print(verschillende_gegevens[-1])
 # --------------------------------------------------------------------------------------------
 
 
 # Maak een tuple met een naam en een leeftijd
 
+student_leeftijd = ("Dopey", 5)
+
+naam, leeftijd = student_leeftijd
+
+print(naam)
+print(leeftijd)
+
 
 # Pak de gegevens uit de tuple en sla ze op in afzonderlijke variabelen (Wat gebeurt er als je de variabelen in de verkeerde volgorde definieert?)
+
 
 
 # Print de uitgepakte variabelen
