0f2d6970bbe61cef3db8a947c29b0009fb8a4a7e
leharteveld <l.e.harteveld@gmail.com>
2026-01-21 11:49:17 +0100

    Les 5 afgerond

---
+++ Les 5/Woorden teller.py
@@ -0,0 +1,27 @@
+# Woorden tellen:
+# programmeer een applicatie die telt hoeveel woorden er in een zin staan.
+# bonus: maak het mogelijk om een zin in te geven en het aantal woorden te tellen.
+#
+# zin = input("Geef een zin waarvan je het aantal woorden wilt: ")
+# aantal_woorden = 1
+#
+# for word in zin:
+#     if word == " ":
+#         aantal_woorden +=1
+#     else:
+#         aantal_woorden +=0
+#
+# print(aantal_woorden)
+
+
+zin = "dit is een testzin om te testen of dit programma werkt"
+aantal_woorden = {}
+
+woorden = zin.split()
+for woord in woorden:
+    if woord in aantal_woorden:
+        aantal_woorden[woord] += 1
+    else:
+        aantal_woorden[woord] = 1
+
+print(aantal_woorden)
--- Les 5/gemiddelde_berekenen.py
+++ Les 5/gemiddelde_berekenen.py
@@ -1,3 +1,6 @@
+#Gemiddelde berekenen:
+#Schrijf een functie die het gemiddelde berekent van een lijst met getallen.
+
 grades = {"Java": 7, "Python": 8, "C#": 6, "HTML": 9, "CSS": 8}
 
 gemiddelde = sum(grades.values()) / len (grades)
