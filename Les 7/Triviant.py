12c283227149b0c33a25717837915281178fe645
leharteveld <l.e.harteveld@gmail.com>
2026-01-21 14:25:45 +0100

    Les 7 aangemaakt

---
+++ Les 7/Triviant.py
@@ -0,0 +1,24 @@
+# Wat is Triviant?
+# We gaan een Triviant spel maken. Triviant is een spelletje waar je vragen moet beantwoorden. Wij gaan een digitale versie maken die in de commandline
+# (terminal) gespeeld kan worden.
+#
+# Randvoorwaarden
+# Je maakt gebruik van de opentdb API.
+# De vragen die je stelt zijn multiple choice vragen vanuit de API, dit kunnen 2 of 4 antwoorden zijn.
+# Stappenplan
+# 1. Kijk op de website van de opentdb API en bekijk hoe je de API kunt gebruiken.
+# 2. Maak een nieuwe python file aan en noem deze triviant.py.
+# 3. Maak een functie get_question() die een vraag ophaalt van de API en deze teruggeeft.
+# Stappenplan voor stap 3
+# 4. Functie voor het printen van vragen
+# Stappenplan voor stap 4
+# 5. Functie voor het vragen van het antwoord van de speler
+# Stappenplan voor stap 5
+# 6. Functie voor het controleren van het antwoord
+# Stappenplan voor stap 6
+# 7. Functie voor het spelen van het spel
+# Stappenplan voor stap 7
+# 8. Functie voor het starten van het spel (main)
+# Stappenplan voor stap 8
+# Bonus
+# Probeer in je spel eens uit hoe je meerdere spelers kunt laten spelen. Hier kun je gebruik maken van de dictionary om de naam van de speler en zijn score bij te houden. De spelers krijgen om de beurt een vraag en kunnen punten verdienen.
