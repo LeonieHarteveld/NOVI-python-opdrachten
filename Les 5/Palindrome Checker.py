981567c44363b6c5e3d276c8b5fc3ccc54c143f0
leharteveld <l.e.harteveld@gmail.com>
2026-01-21 11:21:05 +0100

    Palindrome checker afgerond

--- Les 5/Palindrome Checker.py
+++ Les 5/Palindrome Checker.py
@@ -8,13 +8,42 @@
 #     "lepel": True,
 #     "python": False
 # }
+from dataclasses import replace
+
+# Test in for loop
 
 words = ['radar', 'fiets', 'lepel', 'python']
 
+for word in words:
+    if word == word[::-1]:
+        print("Palindrome")
+    else:
+        print("Geen palindrome")
+
+# Met dictonary
+
+palindrome_words = {
+         "radar": True,
+         "fiets": False,
+         "lepel": True,
+         "python": False
+     }
+for word, boolean in palindrome_words.items():
+    print(f"\nWoord: {word}")
+    print(f"Boolean: {boolean}")
 
 #Bonus
 # 2 zinnen, 1 zin is geen palindrome, de andere wel uncomment de zin die je wilt testen
-zin = "dit is een testzin om te testen of dit programma werkt"
-# zin = "eva can i see bees in a cave"
+zin1 = "dit is een testzin om te testen of dit programma werkt"
+zin2 = "eva can i see bees in a cave"
 
 #Tip 1: Gebruik de replace() functie om alle spaties uit de zin te halen.
+
+def spatie_verwijderen(zin) :
+   zin_zonder_spaties = zin.replace(" ", "")
+   return zin_zonder_spaties == zin_zonder_spaties [::-1]
+
+print(spatie_verwijderen(zin1))
+print(spatie_verwijderen(zin2))
+
+
